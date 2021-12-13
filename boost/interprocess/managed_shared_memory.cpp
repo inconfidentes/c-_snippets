6f4de7d88c6ceb07ab98ef94cf7c99a10023e939
Alberti de Souza Ricardo (ETAS/ERS-DE) <ricardo.alberti-de-souza@etas.com>
2021-12-13 15:23:07 +0100

    Add boost interprocess example for managed shared memory

---
+++ boost/interprocess/managed_shared_memory.cpp
@@ -0,0 +1,50 @@
+// this example is based and adapted from a boost documentation example 
+
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <iostream>
+#include <ostream>
+#include <cstdlib> //std::system
+
+
+int main(int argc, char *argv[])
+{
+    namespace boost_ipc = boost::interprocess;
+    std::cout << "hello computer!"<< std::endl;
+    std::string hello_shm = "hello_shm";
+    
+    if (argc < 2)
+    {
+        struct shm_remove{
+            shm_remove() { boost_ipc::shared_memory_object::remove("My_SHM"); }
+            ~shm_remove(){ boost_ipc::shared_memory_object::remove("My_SHM"); }
+        } remover;
+        
+        std::cout << "first app!" << std::endl;
+
+        boost_ipc::managed_shared_memory segment(boost_ipc::create_only, "My_SHM", 1024);
+        std::string * hello_instance = segment.construct<std::string>
+            ("my_string_instance")
+            (hello_shm);
+
+        
+        
+        std::string s(argv[0]);
+        s += " child ";
+        if(0 != std::system(s.c_str())){
+            return 1;
+        }
+    }
+    else
+    {
+        std::cout << "second app!" << std::endl;
+
+        boost_ipc::managed_shared_memory segment(boost_ipc::open_only, "My_SHM");
+        std::pair<std::string *, boost_ipc::managed_shared_memory::size_type> result;
+        result = segment.find<std::string> ("my_string_instance");
+
+        std::cout << "segment string = " << *result.first << std::endl;
+    }
+    
+    std::cout << "bye now!"<< std::endl;
+    return 0;
+}
